e1da9bc49f1a3ebb32496c6e6f2e3293711b40d9
Marc-Braun-72 <marc.braun72@gmx.de>
2024-09-30 13:44:21 +0200

    main-image added

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,29 +1,28 @@
-
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router'; // Importiere Router für die Navigation
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule], // Import ReactiveFormsModule und CommonModule
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-
 export class ContactComponent implements OnInit {
   contactForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router) {} // Füge Router zum Constructor hinzu
 
   ngOnInit() {
     this.contactForm = this.fb.group({
-      honeypot: [''], // Honeypot-Feld (wird unsichtbar gemacht)
+      honeypot: [''],
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       message: ['', Validators.required],
-      privacyPolicy: [false, Validators.requiredTrue] // Checkbox muss angeklickt sein
+      privacyPolicy: [false, Validators.requiredTrue]
     });
   }
 
@@ -35,5 +34,12 @@ export class ContactComponent implements OnInit {
       console.log('Spam detected or form is invalid');
     }
   }
-}
 
+  // Neue Methode für den Privacy Policy Link
+  openPrivacyPolicy(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    // Hier die Navigation zur Datenschutzerklärung
+    this.router.navigate(['/privacy-policy']);
+  }
+}
